1e4edf8976949c0cc74b95aa8246e68e08a24980
mehdikoubaa <koubaa.mehdi@gmail.com>
2019-12-03 15:02:32 +0100

    upload modif from work comp.

--- Machine learning/ML_tissue_gene_expression.R
+++ Machine learning/ML_tissue_gene_expression.R
@@ -42,3 +42,48 @@ sapply(seq(1, 11, 2), function(k){
 #train fonction with knn
 fit <- train(x, y, method = "knn", tuneGrid = data.frame(k = seq(1,7,2)))
 fit$results
+
+
+#__________Generative models
+#Use the train function to estimate the accuracy of LDA. For this question, use the entire tissue_gene_expression dataset: 
+#do not split it into training and test sets (understand this can lead to overfitting).
+library(tidyverse)
+library(dslabs)
+library(caret)
+data("tissue_gene_expression")
+
+set.seed(1993) #set.seed(1993, sample.kind="Rounding") #if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+fit_lda <- train(x,y,method="lda")
+
+
+#solution
+t(fit_lda$finalModel$means) %>% data.frame() %>%
+  mutate(predictor_name = rownames(.)) %>%
+  ggplot(aes(cerebellum, hippocampus, label = predictor_name)) +
+  geom_point() +
+  geom_text() +
+  geom_abline()
+
+#QDA
+set.seed(1993) #set.seed(1993, sample.kind="Rounding") #if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+fit_qda <- train(x,y,method="qda")
+fit_qda$results$Accuracy
+
+t(fit_qda$finalModel$means) %>% data.frame() %>%
+  mutate(predictor_name = rownames(.)) %>%
+  ggplot(aes(cerebellum, hippocampus, label = predictor_name)) +
+  geom_point() +
+  geom_text() +
+  geom_abline()
+
+fit_lda <- train(x, y, method = "lda", preProcess = c("center"))
+fit_lda$results["Accuracy"]
+       

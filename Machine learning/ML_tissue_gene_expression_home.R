d9d09de882256f412d15e43baba3ee59dc955fae
Mehdi Koubaa <koubaa.mehdi@gmail.com>
2019-12-10 10:02:06 +0100

    update file

--- Machine learning/ML_tissue_gene_expression_home.R
+++ Machine learning/ML_tissue_gene_expression_home.R
@@ -194,3 +194,17 @@ fit_lda$results["Accuracy"]
 # Q1
 d <- dist(tissue_gene_expression$x - rowMeans(tissue_gene_expression$x))
 h <- hclust(d)
+plot(h,cex=0.7)
+
+# Q2
+cl <- kmeans(tissue_gene_expression$x, centers = 7)
+table(cl$cluster, tissue_gene_expression$y)
+
+#Q3
+library(RColorBrewer)
+sds <- matrixStats::colSds(tissue_gene_expression$x)
+ind <- order(sds, decreasing = TRUE)[1:50]
+colors <- brewer.pal(7, "Dark2")[as.numeric(tissue_gene_expression$y)]
+
+heatmap(t(tissue_gene_expression$x[,ind]), col = brewer.pal(11, "RdBu"), scale = "row", ColSideColors = colors)
+heatmap(t(tissue_gene_expression$x[,ind]), col = brewer.pal(11, "RdBu"), scale = "row", ColSideColors = rev(colors))
